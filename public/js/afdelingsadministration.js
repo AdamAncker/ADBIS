d7f6acffb234f2088156947db132a2e3b1f72b86
AdamAncker <91523613+AdamAncker@users.noreply.github.com>
2023-05-21 11:24:06 +0200

    Byttet om på kunder/afdelinger i afdelingsadministration.html

--- public/js/afdelingsadministration.js
+++ public/js/afdelingsadministration.js
@@ -7,43 +7,40 @@ if (!user) {
 }
 
 function showAlert(event) {
-    alert("Tildeling af afdeling(er) udført.");
+    alert("Tildeling af kunde(r) udført.");
     event.preventDefault();
   }
 
-fetchCustomers();
-  
-function fetchCustomers() {
-  fetch(`/api/users/${user.id}/customers`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': '*/*'
-    }
-  }).then(function (response) {
-    response.json().then(function (data) {
-      if (response.status === 200) {
-        let customerDropdownOptions = "";
-        let customerCheckboxes = "";
-        for (let i = 0; i < data.length; i++) {
-          const customerName = data[i].name;
-          customerDropdownOptions += `<option value="${customerName}">${customerName}</option>`;
-          customerCheckboxes += `<div>
-            <input type="checkbox" id="${customerName}" name="customer" value="${customerName}">
-            <label for="${customerName}">${customerName}</label>
-          </div>`;
-        }
-        document.getElementById('customer-dropdown').innerHTML = customerDropdownOptions;
-        document.getElementById('customer-checkboxes').innerHTML = customerCheckboxes;
-      } else {
-        alert(data.message);
+  fetchCustomers();
+
+  function fetchCustomers() {
+    fetch(`/api/users/${user.id}/customers`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': '*/*'
       }
+    }).then(function (response) {
+      response.json().then(function (data) {
+        if (response.status === 200) {
+          let customerCheckboxes = "";
+          for (let i = 0; i < data.length; i++) {
+            const customerName = data[i].name;
+            customerCheckboxes += `<div class="form-check">
+              <input class="form-check-input" type="checkbox" name="customer" id="${customerName}" value="${customerName}">
+              <label class="form-check-label" for="${customerName}">${customerName}</label>
+            </div>`;
+          }
+          document.getElementById('customer-checkboxes').innerHTML = customerCheckboxes;
+        } else {
+          alert(data.message);
+        }
+      });
+    }).catch(function (error) {
+      alert('An unknown error has occurred');
+      console.log(error);
     });
-  }).catch(function (error) {
-    alert('An unknown error has occurred');
-    console.log(error);
-  });
-}
+  }
 
 
 document.getElementById('logout').addEventListener('click', function () {
